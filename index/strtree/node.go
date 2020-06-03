6b410e9e6042725d56962225cb56d792dc9fbef9
Yuhang Chen <i@yuhang.ch>
2020-06-03 10:26:20 +0800

    fii strtree

--- index/strtree/node.go
+++ index/strtree/node.go
@@ -8,10 +8,26 @@ type Node struct {
 	level          int
 }
 
+func NewNode(level int) *Node {
+	return &Node{
+		level: level,
+	}
+}
+
 func (n *Node) computeBounds() *geom.Envelope {
 	return geom.NewEmptyEnvelope()
 }
 
 func (n *Node) Size() int {
 	return len(n.childBoundales)
+}
+
+func (n *Node) appendChildBounds(b interface{}) {
+	n.childBoundales = append(n.childBoundales, b)
+}
+
+type Nodes []*Node
+
+func (n *Nodes) lastNode() *Node {
+	return (*n)[len(*n)-1]
 }
--- index/strtree/str_tree.go
+++ index/strtree/str_tree.go
@@ -7,7 +7,18 @@ const (
 )
 
 type STRTree struct {
+	nodeCapacity int
+
 }
+
+func NewSTRTree(capacity int) *STRTree {
+	if capacity<1 {
+		panic("Node capacity must be greater than 1")
+	}
+	return &STRTree{
+		nodeCapacity: capacity,
+	}
+}pa
 
 func (str *STRTree) verticalSlices(childBoundales Bounds, sliceCount int) []Bounds {
 	cur := 0
@@ -20,5 +31,50 @@ func (str *STRTree) verticalSlices(childBoundales Bounds, sliceCount int) []Boun
 		}
 	}
 	return slices
+}
 
+
+func (str *STRTree) createParentBoundables(childBoundales Bounds, newLevel int) {
+	if len(childBoundales)<0 {
+		panic("空")
+	}
+	leafCount := int(math.Ceil(math.Sqrt(float64(len(childBoundales))/float64(str.nodeCapacity))))
+	var parentBoundales []*Node
+	parentBoundales = append(parentBoundales, NewNode(newLevel))
+	childBoundales.SortY()
+	slices := str.verticalSlices(childBoundales,leafCount)
+	return str.createParentBoundablesFromVerticalSlices(slices,newLevel)
+
+}
+
+func (str *STRTree) createParentBoundablesFromVerticalSlices(vs []Bounds,newLevel int) {
+	if len(vs) < 1 {
+		panic("")
+	}
+	var parentBoundales []*Node
+	for _,v:=range vs {
+		parentBoundales = append(parentBoundales,str.createParentBoundablesFromVerticalSlice(v,newLevel))
+	}
+
+}
+
+func (str *STRTree) createParentBoundablesFromVerticalSlice(b Bounds,newLevel int) {
+	
+}
+
+func (str *STRTree) parentBoundables(childBounds Bounds,newLevel int) Nodes {
+	if len(childBounds)<1 {
+		panic("")
+	}
+	var parentBoundales Nodes
+	parentBoundales = append(parentBoundales, NewNode(newLevel))
+	childBounds.SortY()
+	for _,v:=range childBounds{
+		if len(parentBoundales.lastNode().childBoundales) == str.nodeCapacity {
+			parentBoundales = append(parentBoundales, NewNode(newLevel))
+		}
+		parentBoundales.lastNode().appendChildBounds(v)
+	}
+	return parentBoundales
+	
 }
